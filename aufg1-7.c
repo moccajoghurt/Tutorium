d968dda8953fbfe7c3ce7f1c5fa42d0c6df33730
Marius A. <marius.anderie@hotmail.com>
2013-10-22 15:38:08 +0200

    Create aufg1-7.c

---
+++ aufg1-7.c
@@ -0,0 +1,16 @@
+/* Datei aufg1-7.c */
+/* Umrechnung von Winkelmaß in Bogenmaß */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+	double winkel, bogen;
+	double faktor=2*3.141592654/360;
+	printf("Geben Sie den Winkel im Winkelmass ein: ");
+	scanf("%lf", &winkel);
+	bogen = winkel*faktor;
+	printf("Ein Winkel von %lf Grad entspricht %lf rad\n\n",winkel, bogen);
+	system("Pause");
+}
