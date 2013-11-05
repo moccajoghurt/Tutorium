450b652c4fff1f27b86ae41d976e36abe926541b
Marius A. <marius.anderie@hotmail.com>
2013-11-05 15:43:21 +0100

    Update aufg2-4.c

--- aufg2-4.c
+++ aufg2-4.c
@@ -9,21 +9,21 @@ int main(void) {
     printf("Einkommen eingeben: ");
     scanf("%d", &x);
     
-    if (x < 7665) {
+    if (x <= 7664) {
         steuer = 0;
         
-    } else if (x >= 7665 && x <= 12739) {
+    } else if (x <= 12739) {
         y = (x - 7664) / 10000.;
         steuer = (883.74 * y + 1500) * y;
         
-    } else if (x >= 12740 && x <= 52151) {
+    } else if (x <= 52151) {
         y = (x - 12739) / 10000.;
         steuer = (228.74 * y + 2397) * y + 989;
         
-    } else if (x >= 52152 && x <= 250000) {
+    } else if (x <= 250000) {
         steuer = 0.42 * x - 7914;
         
-    } else if (x > 250000) {
+    } else {
         steuer = 0.45 * x - 15414;
     }
     
