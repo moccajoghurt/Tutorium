6a45d770e878e78124282984b23c685e6f25064f
Marius A. <marius.anderie@hotmail.com>
2013-11-12 15:00:51 +0100

    Update aufg3-2.c

--- aufg3-2.c
+++ aufg3-2.c
@@ -11,30 +11,9 @@ int main(void) {
     scanf("%d", &p);
     
     float sum = 1;
-    int buf = p < 0 ? -p : p;
     
-    do {
-        sum *= z;
-        buf--;
-    } while (buf > 0);
-    sum = p < 0 ? 1/sum : sum;
-    printf("Summe: %f\n", sum);
-    
-    
-    sum = 1;
-    buf = p < 0 ? -p : p;
-    
-    while (buf > 0) {
-        
-        sum *= z;
-        buf--;
-    }
-    sum = p < 0 ? 1/sum : sum;
-    printf("Summe: %f\n", sum);
-    
-    
-    sum = 1;
-    for (buf = p < 0 ? -p : p; buf > 0; buf--) {
+    int i;
+    for (i = p < 0 ? -p : p; i > 0; i--) {
         sum *= z;
     }
     sum = p < 0 ? 1/sum : sum;
