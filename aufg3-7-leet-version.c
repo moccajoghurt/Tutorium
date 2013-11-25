c326bd506bb342717e00a61090f510f7afb4b896
Marius A. <marius.anderie@hotmail.com>
2013-11-25 17:45:40 +0100

    Create aufg3-7-leet-version.c

---
+++ aufg3-7-leet-version.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+	while (1) {
+		char c;
+		unsigned int sec;
+		printf("Sekunden eingeben: ");
+		scanf("%u", &sec);
+		
+		printf("%u Woche%c, ", (sec/60/60/24/7)%4, c = (sec/60/60/24/7)%4 > 1 ? 'n' : '\0');
+		printf("%u Tag%c, ", (sec/60/60/24)%7, c = (sec/60/60/24)%7 > 1 ? 'e' : '\0');
+		printf("%02u:", (sec/60/60)%24);
+		printf("%02u:", (sec/60)%60);
+		printf("%02u Stunden\n", sec%60);
+	}
+}
