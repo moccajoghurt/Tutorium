688e4ecbb7aaa4ba0e52182e22c4f1a2d9936a37
Marius A. <marius.anderie@hotmail.com>
2013-11-25 17:55:31 +0100

    Update aufg3-7-leet-version.c

--- aufg3-7-leet-version.c
+++ aufg3-7-leet-version.c
@@ -10,8 +10,6 @@ int main() {
 		
 		printf("%u Woche%c, ", (sec/60/60/24/7)%4, c = (sec/60/60/24/7)%4 > 1 ? 'n' : '\0');
 		printf("%u Tag%c, ", (sec/60/60/24)%7, c = (sec/60/60/24)%7 > 1 ? 'e' : '\0');
-		printf("%02u:", (sec/60/60)%24);
-		printf("%02u:", (sec/60)%60);
-		printf("%02u Stunden\n", sec%60);
+		printf("%02u:%02u:%02u Stunden\n", (sec/60/60)%24, (sec/60)%60, sec%60);
 	}
 }
